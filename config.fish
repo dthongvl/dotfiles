f88a638457eda68a9d6101e17d3a7d27bf635d35
Thong Nguyen <thong_pro113520@yahoo.com>
2020-02-27 10:40:15 +0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,4 +1,5 @@
 set -x ORACLE_HOME /opt/oracle/
 set -x LD_LIBRARY_PATH /opt/oracle/instantclient
 
-set -gx PATH $PATH $HOME/.rvm/bin (yarn global bin)
+set -gx PATH $PATH $HOME/.rvm/bin (yarn global bin) /usr/local/go/bin
+set fish_greeting 'No Sana No Life'
