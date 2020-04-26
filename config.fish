9c60d9992ae1d276fe50044dfaf79a6c00333f15
Thong Nguyen <thong_pro113520@yahoo.com>
2020-04-26 13:15:23 +0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,3 +1,10 @@
+abbr -a e nvim
+abbr -a o xdg-open
+abbr -a g git
+abbr -a gc 'git checkout'
+abbr -a ga 'git add -p'
+abbr -a gs 'git status'
+
 set -x ORACLE_HOME /opt/oracle/
 set -x LD_LIBRARY_PATH /opt/oracle/instantclient
 
