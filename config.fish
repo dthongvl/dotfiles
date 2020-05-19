106de9c0222d6e5e6058a8c4c856be3f6161ac78
Thong Nguyen <thong_pro113520@yahoo.com>
2020-05-19 18:02:00 +0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -8,5 +8,7 @@ abbr -a gs 'git status'
 set -x ORACLE_HOME /opt/oracle/
 set -x LD_LIBRARY_PATH /opt/oracle/instantclient
 
-set -gx PATH $PATH $HOME/.rvm/bin (yarn global bin) /usr/local/go/bin
+set -gx PATH $PATH $HOME/.rbenv/bin (yarn global bin) /usr/local/go/bin
 set fish_greeting 'No Sana No Life'
+
+status --is-interactive; and rbenv init - | source
