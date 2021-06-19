8a5e627d7ae96ce4595c16266c5a0232272aae7a
dthongvl <thong_pro113520@yahoo.com>
2021-06-19 13:28:38 +0700

    Update fish config

--- config.fish
+++ config.fish
@@ -1,15 +1,19 @@
 abbr -a e nvim
 abbr -a o xdg-open
 abbr -a g git
-abbr -a gc 'git checkout'
+abbr -a gco 'git checkout'
+abbr -a gcom 'git checkout master'
 abbr -a ga 'git add -p'
 abbr -a gs 'git status'
+abbr -a gd 'git diff'
+abbr -a gcm 'git commit -m "'
 
 set -x ORACLE_HOME /opt/oracle/
 set -x LD_LIBRARY_PATH /opt/oracle/instantclient
-set -x JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64
 
-set -gx PATH $PATH $HOME/.rbenv/bin (yarn global bin) /usr/local/go/bin $HOME/.local/bin $JAVA_HOME/bin
+set -gx PATH $PATH $HOME/.rbenv/bin (yarn global bin) /usr/local/go/bin $HOME/.local/bin $HOME/.cargo/bin
 set fish_greeting 'No Sana No Life'
 
 status --is-interactive; and rbenv init - | source
+
+if test -f /home/dthongvl/.autojump/share/autojump/autojump.fish; . /home/dthongvl/.autojump/share/autojump/autojump.fish; end
