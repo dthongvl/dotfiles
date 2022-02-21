25865b6912bce2f122eb6ce0f0eee732d9078c01
Thong Nguyen <matong.mz@gmail.com>
2022-02-21 19:47:41 +0700

    chore(setup): update setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,30 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+# Fishshell
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt install fish
+
+# Set fishshell to default shell
+chsh -s /usr/bin/fish
+
+# Nodejs
+sudo apt install npm
+# make cache folder (if missing) and take ownership
+sudo mkdir -p /usr/local/n
+sudo chown -R $(whoami) /usr/local/n
+# make sure the required folders exist (safe to execute even if they already exist)
+sudo mkdir -p /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+# take ownership of Node.js install destination folders
+sudo chown -R $(whoami) /usr/local/bin /usr/local/lib /usr/local/include /usr/local/share
+
+# Install yarn
+npm install -g yarn
+yarn global add n
+
 # Packages
-sudo apt install alacritty curl jq ripgrep ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip rbenv
+sudo apt install alacritty tmux curl jq ripgrep ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip rbenv
 yarn global add vscode-html-languageserver-bin typescript typescript-language-server vscode-css-languageserver-bin dockerfile-language-server-nodejs vscode-json-languageserver vls vim-language-server yaml-language-server @volar/server
 
 # Config files
@@ -10,8 +32,10 @@ ln -s -f ~/workspace/dotfiles/nvim ~/.config
 
 ln -s -f ~/workspace/dotfiles/.tmux.conf ~/.tmux.conf
 
-ln -s -f ~/workspace/dotfiles/config.fish ~/.config/fish
+mkdir ~/.config/fish
+ln -s -f ~/workspace/dotfiles/config.fish ~/.config/fish/config.fish
 
+mkdir ~/.config/alacritty
 ln -s -f ~/workspace/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 # Jump
@@ -21,12 +45,13 @@ wget https://github.com/gsamokovarov/jump/releases/download/v0.40.0/jump_0.40.0_
 git clone https://github.com/neovim/neovim ~/workspace/neovim
 (cd ~/workspace/neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo -j4 && sudo make install)
 
-# Nodejs
-npm install -g yarn
-yarn global add n
+# Packer for Neovim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+mkdir ~/.local/bin
 curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
 chmod +x ~/.local/bin/rust-analyzer
 rustup component add rust-src
